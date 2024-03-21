fa629407b5ed91ab163a560989c627bef13dd246
laughter1998 <laughter98@hanmail.net>
2024-03-21 17:14:53 +0900

    택택토 연스 0321

---
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const handleClickBoard = (row, col) => {
+    console.log(`${row} , ${col}`);
+    // initialGameBoard[row][col] = "x";
+  };
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleClickBoard(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleClickEdit = () => {
+    setIsEditing((editing) => !editing);
+    // setIsEditing(!isEditing);
+    // setIsEditing(!isEditing);
+  };
+  const handleChangeInput = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} onChange={handleChangeInput} />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClickEdit}>{btnCaption}</button>
+    </li>
+  );
+}
